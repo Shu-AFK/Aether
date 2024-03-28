7d793e1501a1cf44cd65a0e469a4a82de33f90ed
floyd <heissewie@gmail.com>
2024-03-28 21:45:00 +0100

    Added printing to the kernel

--- src/kernel.c
+++ src/kernel.c
@@ -1,5 +1,64 @@
 #include "kernel.h"
 
-void kernel_main() {
+uint16_t *video_mem;
+uint16_t terminal_row = 0;
+uint16_t terminal_col = 0;
 
+uint16_t terminal_make_char(char c, char color) {
+    return (color << 8) | c;
+}
+
+void terminal_putchar(int x, int y, char c, char color) {
+    video_mem[(y * VGA_WIDTH) + x] = terminal_make_char(c, color);
+}
+
+void terminal_writechar(char c, char color) {
+    if(c == '\n') {
+        terminal_col = 0;
+        terminal_row++;
+        return;
+    }
+
+    terminal_putchar(terminal_col, terminal_row, c, color);
+
+    terminal_col++;
+    if(terminal_col >= VGA_WIDTH) {
+        terminal_col = 0;
+        terminal_row++;
+    }
+}
+
+void terminal_initialise() {
+    video_mem = (uint16_t *)(0xB8000);
+    terminal_col = 0;
+    terminal_row = 0;
+
+    for(int i = 0; i < VGA_HEIGHT; i++) {
+        for(int j = 0; j < VGA_WIDTH; j++) {
+            video_mem[(i * VGA_WIDTH) + j] = terminal_make_char(' ', 0);
+        }
+    }
+}
+
+int strlen(char *str) {
+    int counter = 0;
+    while(str[counter] != '\0')
+        counter++;
+
+    return counter;
+}
+
+int print_str(char *str, char color) {
+    int len = strlen(str);
+
+    for(int i = 0; i < len; i++) {
+        terminal_writechar(str[i], color);
+    }
+
+    return 0;
+}
+
+void kernel_main() {
+    terminal_initialise();
+    print_str("Hello, world!\ntest", 15);
 }
--- src/kernel.h
+++ src/kernel.h
@@ -1,6 +1,11 @@
 #ifndef AETHER_KERNEL_H
 #define AETHER_KERNEL_H
 
+#include <stdint.h>
+
+#define VGA_WIDTH 80
+#define VGA_HEIGHT 20
+
 void kernel_main();
 
 #endif
