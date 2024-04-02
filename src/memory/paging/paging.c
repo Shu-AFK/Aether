b6e5ff30b7aa4c253cfaf411335c2237969fae54
floyd <heissewie@gmail.com>
2024-04-02 11:59:48 +0200

    Added page mapping

--- src/memory/paging/paging.c
+++ src/memory/paging/paging.c
@@ -1,5 +1,6 @@
 #include "paging.h"
 #include "memory/heap/kheap.h"
+#include "status.h"
 
 static uint32_t *current_directory = 0;
 
@@ -30,4 +31,41 @@ void paging_switch(uint32_t *directory) {
 
 uint32_t *paging_4gb_chunk_get_directory(struct paging_4gb_chunk *chunk) {
     return chunk->directory_entry;
+}
+
+bool paging_is_aligned(void *addr) {
+    return ((uint32_t) addr % PAGING_PAGE_SIZE) == 0;
+}
+
+int paging_get_indexes(void *virtual_addr, uint32_t *directory_index_out, uint32_t *table_index_out) {
+    int res = 0;
+    if(!paging_is_aligned(virtual_addr)) {
+        res = -EINVARG;
+        goto out;
+    }
+
+    *directory_index_out = ((uint32_t) virtual_addr / (PAGING_TOTAL_ENTRIES_PER_TABLE * PAGING_PAGE_SIZE));
+    *table_index_out = ((uint32_t) virtual_addr % (PAGING_TOTAL_ENTRIES_PER_TABLE * PAGING_PAGE_SIZE) / PAGING_PAGE_SIZE);
+
+out:
+    return res;
+}
+
+int paging_set(uint32_t *directory, void *virtual_addr, uint32_t val) {
+    if(!paging_is_aligned(virtual_addr)) {
+        return -EINVARG;
+    }
+
+    uint32_t directory_index = 0;
+    uint32_t table_index = 0;
+    int res = paging_get_indexes(virtual_addr, &directory_index, &table_index);
+    if(res < 0) {
+        return res;
+    }
+
+    uint32_t entry = directory[directory_index];
+    uint32_t *table = (uint32_t *) (entry & 0xfffff000); // Extracting the page table address
+    table[table_index] = val;
+
+    return 0;
 }
