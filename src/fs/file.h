f089ef3b970248376dc0af32664d6a6ce5c42a29
floyd <heissewie@gmail.com>
2024-04-10 23:15:40 +0200

    Implemented generic VFS fread

--- src/fs/file.h
+++ src/fs/file.h
@@ -1,6 +1,7 @@
 #ifndef AETHER_FILE_H
 #define AETHER_FILE_H
 
+#include <stdint.h>
 #include "pparser.h"
 
 typedef unsigned int FILE_SEET_MODE;
@@ -20,12 +21,14 @@ enum {
 
 struct disk;
 typedef void *(*FS_OPEN_FUNCTION)(struct disk *disk, struct path_part *path, FILE_MODE mode_str);
+typedef int (*FS_READ_FUNCTION)(struct disk *disk, void *private, uint32_t size, uint32_t nmemb, char *out);
 typedef int(*FS_RESOLVE_FUNCTION)(struct disk *disk);
 
 struct filesystem {
     // Filesystem should return 0 form resolve if the provided disk is using its filesystem
     FS_RESOLVE_FUNCTION resolve;
     FS_OPEN_FUNCTION open;
+    FS_READ_FUNCTION read;
 
     char name[20];
 };
@@ -44,6 +47,8 @@ struct file_descriptor {
 
 void fs_init();
 int fopen(const char *filename, const char *mode_str);
+int fread(void *ptr, uint32_t size, uint32_t nmemb, int fd);
+
 void fs_insert_filesystem(struct filesystem *filesystem);
 struct filesystem *fs_resolve(struct disk *disk);
 
